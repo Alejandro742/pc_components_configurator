c46d1f9c420405f1c434a93a8af686ec072fb906
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-18 14:08:57 -0500

    Link to google fonts and atylessheet

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PC Components</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/styles.css">
 </head>
 
 <body>
