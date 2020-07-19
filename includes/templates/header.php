af744161242308dd4c869bc2f9680d70e32fc191
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-18 20:12:27 -0500

    Font Awesome CDN

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PC Components</title>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/cc1f853ce1.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="public/styles.css">
 </head>
 
