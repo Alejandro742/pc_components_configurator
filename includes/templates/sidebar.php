0ff8ea791881a4ad571a25594647f96b1ba303ec
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-18 19:56:25 -0500

    Sidebar HTML done

---
+++ includes/templates/sidebar.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+    <h2>Agregar PC</h2>
+    <form action="#">
+        <div class="input-group">
+            <label for="pc_name">Nombre</label>
+            <input type="text" for="pc_name" placeholder="Nombre PC">
+        </div>
+        <div class="input-group">
+            <label for="pc_desc">Descripción</label>
+            <textarea name="pc_desc"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Agregar PC</button>
+    </form>
+</div>
