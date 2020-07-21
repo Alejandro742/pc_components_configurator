65d5d2b7ab961732d17f62cecd92c76e75def634
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-21 01:15:31 -0500

    minor changes in id's

--- includes/templates/sidebar.php
+++ includes/templates/sidebar.php
@@ -1,20 +1,20 @@
 <div class="sidebar">
     <h2>Agregar PC</h2>
-    <form action="#">
+    <form action="#" id="form_pc">
         <div class="input-group">
             <label for="pc_name">Nombre</label>
-            <input type="text" for="pc_name" placeholder="Nombre PC">
+            <input type="text" for="pc_name" id="pc_name" placeholder="Nombre PC">
         </div>
         <div class="input-group">
             <label for="pc_desc">Descripción</label>
-            <textarea name="pc_desc"></textarea>
+            <textarea name="pc_desc" id="pc_desc"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary btn-block">Agregar PC</button>
+        <input type="submit" class="btn btn-primary btn-block" value="Agregar PC">
     </form>
-    <ul>
+    <ul id ="computers-list">
         <li>
            <a href="index.php?pc_id:1">PC MASTER 01</a> 
-           <i class="far fa-trash-alt"></i>
+           <i id = "999" class="far fa-trash-alt"></i>
         </li>
     </ul>
 </div>
