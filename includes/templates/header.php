e7d10c24147106af63b2325dee9589fc04b17627
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-20 23:30:16 -0500

    Sweet Alert

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -7,6 +7,7 @@
     <title>PC Components</title>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/cc1f853ce1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="public/sweetalert2.min.css">
     <link rel="stylesheet" href="public/styles.css">
 </head>
 
