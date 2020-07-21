57f18f552ecf108d76fe9b2ea4fe95a4300d70aa
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-20 23:30:27 -0500

    SideBar components view

---
+++ includes/templates/sidebarComponents.php
@@ -0,0 +1,25 @@
+<div class="sidebar">
+    <h2>Agregar Componente</h2>
+    <form action="#">
+        <div class="input-group">
+            <label for="component_name">Nombre</label>
+            <input type="text" for="component_name" placeholder="Nombre Componente">
+        </div>
+        <div class="input-group">
+            <label for="component_desc">Descripción</label>
+            <textarea name="component_desc"></textarea>
+        </div>
+        <div class="input-group">
+            <label for="component_price">Precio</label>
+            <input type="number" name="component_price" placeholder="Precio">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Agregar Componente</button>
+    </form>
+    <h3><a href="index.php">--- TUS PC's ---</a></h3>
+    <ul>
+        <li>
+            <a href="index.php?pc_id:1">PC MASTER 01</a>
+            <i class="far fa-trash-alt"></i>
+        </li>
+    </ul>
+</div>
