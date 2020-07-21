68d4e65bb09910d3c9642a36f60bcd33d408e7b0
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-20 23:30:04 -0500

    Conditional to know what js script charge

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,4 +1,12 @@
+<script src="js/sweetalert2.all.min.js"></script>
+<?php
+    if(verifyOpenProject()){
+        echo " <script src = 'js/components.js'></script> ";
+    }else{
+        echo " <script src = 'js/computers.js'></script> ";
+    }
+
+?>
 
 </body>
-
 </html>
