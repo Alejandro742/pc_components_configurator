f03c70b30104655c3765524bf37fb7e40b3a74f4
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-22 02:22:33 -0500

    HTML fot dynamic components list

--- index.php
+++ index.php
@@ -18,17 +18,35 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt similique aspernatur. Veniam iure quae quisquam id ullam error delectus labore obcaecati, laboriosam, velit sequi ut officia repellat ratione repudiandae?</p>
             <hr>
             <div id="components-list"class="components-list">
-                <div class="item-component">
+                <div class="item-component" id="item-component">
                     <h4>NAME</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sint eaque quibusdam.</p>
                     <p class="price">$1000</p>
+                    <i class='far fa-trash-alt'></i>
                 </div>
                 <hr>
-                <div class="item-component">
-                    <h4>NAME2</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sint eaque quibusdam.</p>
-                    <p class="price">$2000</p>
-                </div>
+
+                <?php
+                    $id_pc = getIdComputerIdInURL();
+                    $components = getComponents($id_pc);
+                    if(!$components){
+                ?>
+                        <p>No hay componentes para mostrar</p>
+                <?php
+                    }else{
+                        foreach($components as $component){
+                ?>
+                            <div class="item-component" id="<?php echo $component["component_id"]; ?>">
+                                <h4><?php echo $component["component_name"];?></h4>
+                                <p><?php echo $component["component_desc"];?></p>
+                                <p class="price"><?php echo $component["component_price"];?></p>
+                                <i class='far fa-trash-alt'></i>
+                            </div>
+                <?php
+                        }
+                    }
+                ?>
+
             </div>
         </main>
     </div>
