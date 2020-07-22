6669942150438f7acb37d34e57c8a74b47ba8e7c
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-22 03:40:57 -0500

    Index view finished

--- index.php
+++ index.php
@@ -8,47 +8,54 @@
         <?php
             if(verifyOpenProject()){
                 include './includes/templates/sidebarComponents.php';
-            }else{
-                include './includes/templates/sidebar.php';
-            }
-
         ?>
-        <main class="components-container">
-            <h2>PC MASTER 0<?php echo $pc_id?></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt similique aspernatur. Veniam iure quae quisquam id ullam error delectus labore obcaecati, laboriosam, velit sequi ut officia repellat ratione repudiandae?</p>
-            <hr>
-            <div id="components-list"class="components-list">
-                <div class="item-component" id="item-component">
-                    <h4>NAME</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sint eaque quibusdam.</p>
-                    <p class="price">$1000</p>
-                    <i class='far fa-trash-alt'></i>
-                </div>
-                <hr>
-
+                <main class="components-container">
                 <?php
                     $id_pc = getIdComputerIdInURL();
-                    $components = getComponents($id_pc);
-                    if(!$components){
+                    $computer = getComputer($id_pc);
+                    foreach($computer as $data){
                 ?>
-                        <p>No hay componentes para mostrar</p>
+                    <h2><?php echo $data["pc_name"];?></h2>
+                    <p><?php echo $data["pc_desc"];?></p>
                 <?php
-                    }else{
-                        foreach($components as $component){
+                    } 
                 ?>
-                            <div class="item-component" id="<?php echo $component["component_id"]; ?>">
-                                <h4><?php echo $component["component_name"];?></h4>
-                                <p><?php echo $component["component_desc"];?></p>
-                                <p class="price"><?php echo $component["component_price"];?></p>
-                                <i class='far fa-trash-alt'></i>
-                            </div>
-                <?php
+                <hr>
+                <div id="components-list"class="components-list">
+                    <hr>
+    
+                    <?php
+                        $components = getComponents($id_pc);
+                        if($components->num_rows===0){
+                    ?>
+                            <p class="msg-componente">No hay componentes para mostrar</p>
+                    <?php
+                        }else{
+                            foreach($components as $component){
+                    ?>
+                                <div class="item-component" id="<?php echo $component["component_id"]; ?>">
+                                    <h4><?php echo ($component["component_name"]);?></h4>
+                                    <p><?php echo $component["component_desc"];?></p>
+                                    <p class="price"><?php echo $component["component_price"];?></p>
+                                    <i class='far fa-trash-alt'></i>
+                                </div>
+                    <?php
+                            }
                         }
-                    }
-                ?>
+                    ?>
+    
+                </div>
+            </main>
+        
+        <?php
+            }else{
+                include './includes/templates/sidebar.php';
+        ?>
+                <p class="msg-pc mt-1r">No hay computadora para mostrar</p>
+        <?php
+            }
+        ?>
 
-            </div>
-        </main>
     </div>
 
 
