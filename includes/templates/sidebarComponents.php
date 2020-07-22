7dc9451dfd26f153d3e03b3e4fcf8d6b715fc278
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-22 01:37:34 -0500

    adding id's to inputs

--- includes/templates/sidebarComponents.php
+++ includes/templates/sidebarComponents.php
@@ -1,19 +1,19 @@
 <div class="sidebar">
     <h2>Agregar Componente</h2>
-    <form action="#">
+    <form action="#" id="components_form">
         <div class="input-group">
             <label for="component_name">Nombre</label>
-            <input type="text" for="component_name" placeholder="Nombre Componente">
+            <input type="text" for="component_name" id="component_name" placeholder="Nombre Componente">
         </div>
         <div class="input-group">
             <label for="component_desc">Descripción</label>
-            <textarea name="component_desc"></textarea>
+            <textarea name="component_desc" id="component_desc"></textarea>
         </div>
         <div class="input-group">
             <label for="component_price">Precio</label>
-            <input type="number" name="component_price" placeholder="Precio">
+            <input type="number" name="component_price" id="component_price" placeholder="Precio">
         </div>
-        <button type="submit" class="btn btn-primary btn-block">Agregar Componente</button>
+        <input type="submit" class="btn btn-primary btn-block" value="Agregar Componente">
     </form>
     <h3><a href="index.php">--- TUS PC's ---</a></h3>
     <?php include "./includes/layouts/computers_list.php"?>
