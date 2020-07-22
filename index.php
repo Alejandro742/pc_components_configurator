3025967bb7939239eba6805f8cc2e9e80af3b0ad
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-22 01:37:05 -0500

    provitional template for components list

--- index.php
+++ index.php
@@ -13,11 +13,22 @@
             }
 
         ?>
-        <main class="components-container container">
+        <main class="components-container">
             <h2>PC MASTER 0<?php echo $pc_id?></h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt similique aspernatur. Veniam iure quae quisquam id ullam error delectus labore obcaecati, laboriosam, velit sequi ut officia repellat ratione repudiandae?</p>
-            <div class="components">
-
+            <hr>
+            <div id="components-list"class="components-list">
+                <div class="item-component">
+                    <h4>NAME</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sint eaque quibusdam.</p>
+                    <p class="price">$1000</p>
+                </div>
+                <hr>
+                <div class="item-component">
+                    <h4>NAME2</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sint eaque quibusdam.</p>
+                    <p class="price">$2000</p>
+                </div>
             </div>
         </main>
     </div>
