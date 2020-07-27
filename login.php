d704002e57e6e34d9f330c2d92a218bd4026126c
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-27 01:54:37 -0500

    form login

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+include 'includes/templates/header.php';
+?>
+
+<div class="center-form">
+  <h1>Login</h1>
+  <div class="form-container">
+    <form action="#" id="users_form" method="POST">
+      <div class="input-group">
+        <label for="email">Nombre de Usuario</label>
+        <input type="email"  for="email" id="email">
+      </div>
+      <div class="input-group">
+        <label for="password">Contraseña</label>
+        <input type="password" for="password" id="password">
+      </div>
+      <input type="hidden" id="tipo" value="signup">
+      <input type="submit" class="btn btn-block btn-primary" value="Iniciar Sesión">
+    </form>
+    <a href="signup.php">Crear Cuenta</a>
+  </div>
+</div>
+
+<?php
+include 'includes/templates/footer.php';
+?>
