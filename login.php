9de5448123a66f30c7fd2d120de453c6f1227d47
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-27 03:23:45 -0500

    correcciones menores

--- login.php
+++ login.php
@@ -7,14 +7,14 @@ include 'includes/templates/header.php';
   <div class="form-container">
     <form action="#" id="users_form" method="POST">
       <div class="input-group">
-        <label for="email">Nombre de Usuario</label>
+        <label for="email">Correo</label>
         <input type="email"  for="email" id="email">
       </div>
       <div class="input-group">
         <label for="password">Contraseña</label>
         <input type="password" for="password" id="password">
       </div>
-      <input type="hidden" id="tipo" value="signup">
+      <input type="hidden" id="tipo" value="login">
       <input type="submit" class="btn btn-block btn-primary" value="Iniciar Sesión">
     </form>
     <a href="signup.php">Crear Cuenta</a>
