c7e8d132a40a1657d59588e0751f646af379eadb
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-27 01:54:43 -0500

    form signup

---
+++ signup.php
@@ -0,0 +1,30 @@
+<?php
+include 'includes/templates/header.php';
+?>
+
+<div class="center-form">
+  <h1>Sign Up</h1>
+  <div class="form-container">
+    <form action="#" id="users_form" method="POST">
+      <div class="input-group">
+        <label for="username">Nombre de Usuario</label>
+        <input type="text"  for="username" id="username">
+      </div>
+      <div class="input-group">
+        <label for="email">Correo</label>
+        <input type="email"  for="email" id="email">
+      </div>
+      <div class="input-group">
+        <label for="password">Contraseña</label>
+        <input type="password" for="password" id="password">
+      </div>
+      <input type="hidden" id="tipo" value="login">
+      <input type="submit" class="btn btn-block btn-primary" value="Iniciar Sesión">
+    </form>
+    <a href="login.php">Iniciar sesión</a>
+  </div>
+</div>
+
+<?php
+include 'includes/templates/footer.php';
+?>
