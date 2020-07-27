ce729355ef100011a848ad0f28c20f71b56afa9a
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-27 01:55:30 -0500

    Conditional to add the form js script to the footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,9 +1,20 @@
 <script src="js/sweetalert2.all.min.js"></script>
 <?php
-    if(verifyOpenProject()){
-        echo " <script src = 'js/components.js'></script> ";
+    // if(verifyOpenProject()){
+    //     echo " <script src = 'js/components.js'></script> ";
+    // }else{
+    //     echo " <script src = 'js/computers.js'></script> ";
+    // }
+    $archivo = basename($_SERVER['PHP_SELF']);
+    $pagina = str_replace(".php","",$archivo);
+    if($pagina==="login" || $pagina === "signup"){
+        echo " <script src = 'js/users.js'></script> ";
     }else{
-        echo " <script src = 'js/computers.js'></script> ";
+        if(verifyOpenProject()){
+            echo " <script src = 'js/components.js'></script> ";
+        }else{
+            echo " <script src = 'js/computers.js'></script> ";
+        }
     }
 
 ?>
